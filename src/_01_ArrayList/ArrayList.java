fadd1e36e78ad13a5a43c72c5c57d130d1b29263
LeeHuiJu <leemoon63@gmail.com>
2022-05-09 21:35:25 +0900

    ArrayList_set, get, remove, size, isEmpty, indexOf, contains, clear 추가

--- src/_01_ArrayList/ArrayList.java
+++ src/_01_ArrayList/ArrayList.java
@@ -22,23 +22,26 @@ public class ArrayList {
 		return newArray;		
 	}
 	
-	//정수값을 매개변수로 받아 리스트 맨 뒤에 삽입
+	//매개변수를 받아 리스트 삽입
 	public boolean add(Object value) {
 		addLast(value);
 		return true;
 	}
-
+	
+	//매개변수를 받아 리스트 맨 뒤에 삽입
 	public void addLast(Object value) {
 		if (size == listArray.length) {
 			growSize(listArray);
 		}
 		listArray[size++] = value;
 	}
-
+	
+	//매개변수를 받아 리스트 맨 앞에 삽입
 	public void addFirst(Object value) {
 		add(0, value);
 	}
 
+	//매개변수를 받아 리스트 특정 위치에 삽입
 	public void add(int index, Object value) {
 		if (index > size || index < 0) {
 			throw new IndexOutOfBoundsException();
@@ -57,6 +60,76 @@ public class ArrayList {
 		}
 		listArray[index] = value;
 		size++;
+	}
+	
+	//특정 위치의 값을 변경
+	public void set(int index, Object value) {
+		if(index>=size || index<0)
+			throw new IndexOutOfBoundsException();
+		else
+			listArray[index]=value;
+	}
+	
+	//특정 위치의 값을 반환
+	public Object get(int index) {
+		if(index>=size || index<0)
+			throw new IndexOutOfBoundsException();
+		else 
+			return listArray[index];
+	}
+	
+	//특정 index의 데이터 삭제하고 리턴
+	public Object remove(int index) {
+		if(index>=size || index<0)
+			throw new IndexOutOfBoundsException();
+		Object value=listArray[index];
+		
+		for (int i=index; i<size-1; i++) {
+			listArray[i] = listArray[i + 1];
+		}
+		listArray[size--]=null;
+		return value;
+	}
+	
+	//특정 데이터를 삭제하고 성공 유무 리턴
+	public boolean remove(Object value) {
+		int index = indexOf(value);
+		if (index == -1) {
+			return false;
+		}
+		remove(index);
+		return true;
+	}
+
+	//저장된 데이터의 수 리턴
+	public int size() {
+		return size;
+	}
+	
+	//데이터의 유무 리턴
+	public boolean isEmpty() {
+		return size==0;
+	}
+	
+	//원하는 데이터의 index를 리턴
+	public int indexOf(Object value) {
+		for(int i=0; i<size; i++) {
+			if(value.equals(listArray[i]))
+				return i;
+		}
+		return -1;
+	}
+	
+	//원하는 데이터의 유무 리턴
+	public boolean contains(Object value) {
+		return indexOf(value) >= 0;
+	}
+	
+	//저장된 데이터 모두 삭제
+	public void clear() {
+		for (int i=0; i<size; i++)
+			listArray[i] = null;
+		size = 0;
 	}
 	
 }
