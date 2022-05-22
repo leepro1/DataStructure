501567069c251c482477e3cb3af51d3125c138ba
LeeHuiJu <leemoon63@gmail.com>
2022-05-22 17:31:55 +0900

    Stack 구현

--- src/_04_Stack/ArrayStack.java
+++ src/_04_Stack/ArrayStack.java
@@ -3,37 +3,66 @@ package _04_Stack;
 import java.util.EmptyStackException;
 
 public class ArrayStack {
-	private int[] stackArray; //정수 원소를 저장할 배열
-	private int top; //꼭데기 원소의 위치 변수
+	private Object[] stackArray;
+	private int top;
 	
 	public ArrayStack(int capacity) {
-		stackArray=new int[capacity];
+		stackArray=new Object[capacity];
 		top=-1;
 	}
 	
-	public boolean isEmpty() { //스택이 비었는지 검사
+	public boolean empty() {
 		return top==-1;
 	}
 	
-	public boolean isFull() { //스택이 가득 찼는지 검사
+	private boolean isFull() {
 		return top==stackArray.length-1;
 	}
 	
-	public void push(int item) { //스택 top에 원소 삽입
+	public Object push(Object item) {
 		if(isFull())
 			stackArray=grow(stackArray);
 		stackArray[++top]=item;
+		
+		return item;
 	}
 	
-	public int pop() { //스택 top에서 원소를 삭제하여 리턴
-		if(isEmpty())
+	public Object peek() {
+		if(empty())
 			throw new EmptyStackException();
-		else
-			return stackArray[top--];
+		return stackArray[top];
+	}
+	
+	public Object pop() {
+		if(empty())
+			throw new EmptyStackException();
+		else {
+			Object temp=stackArray[top];
+			stackArray[top--]=null;
+			return temp;
+		}
+	}
+	
+	public int size() {
+		return top+1;
+	}
+	
+	public int search(Object item) {
+		for(int temp=top;temp>=0;temp--) {
+			if(stackArray[temp].equals(item))
+				return top - temp + 1;
+		}
+		return -1;
 	}
 
-	public int[] grow(int[] oldArray) { //배열의 크기를 2배로 만드는 메서드
-		int[] newArray=new int[oldArray.length*2];
+	public void clear() {
+		for(int i=0;i<=top;i++)
+			stackArray[i]=null;
+		top=-1;
+	}
+	
+	public Object[] grow(Object[] oldArray) {
+		Object[] newArray=new Object[oldArray.length*2];
 			for (int i = 0; i < oldArray.length; i++) {
 				newArray[i] = oldArray[i];
 			}
