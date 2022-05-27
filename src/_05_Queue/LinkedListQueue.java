5ff1bb91370f3b5abd670ed77b28a58b59e628c9
LeeHuiJu <leemoon63@gmail.com>
2022-05-27 10:01:05 +0900

    LinkedListQueue

--- src/_05_Queue/LinkedListQueue.java
+++ src/_05_Queue/LinkedListQueue.java
@@ -19,39 +19,75 @@ public class LinkedListQueue {
 	}
 	
 	public boolean add(Object item) {
-		//return true;
+		Node newNode = new Node();
+		newNode.data=item;
+		
+		if(head==null)
+			head=newNode;
+		else
+			tail.link=newNode;
+		
+		tail=newNode;
+		size++;
+		return true;
 	}
 	
 	public boolean offer(Object item) {
-		//return true;
+		Node newNode = new Node();
+		newNode.data=item;
+		
+		if(head==null)
+			head=newNode;
+		else
+			tail.link=newNode;
+		
+		tail=newNode;
+		size++;
+		return true;
 	}
 	
 	public Object remove() {
 		if(isEmpty())
 			throw new NoSuchElementException();
-		//return
+		
+		Object item=head.data;
+		Node newNode=head.link;
+		head.data=null;
+		head.link=null;
+		head=newNode;
+		size--;
+		
+		return item;
 	}
 	
 	public Object poll() {
 		if(isEmpty())
 			return null;
-		//return
+		
+		Object item=head.data;
+		Node newNode=head.link;
+		head.data=null;
+		head.link=null;
+		head=newNode;
+		size--;
+		
+		return item;
 	}
 	
 	public Object element() {
 		if(isEmpty())
 			throw new NoSuchElementException();
-		//return
+		return head.data;
 	}
 	
 	public Object peek() {
 		if(isEmpty())
 			return null;
-		//return 
+		return head.data; 
 	}
 	
 	public boolean isEmpty() {
-		return head==null; //
+		return head==null;
 	}
 	
 	public int size() {
@@ -59,12 +95,21 @@ public class LinkedListQueue {
 	}
 	
 	public boolean contains(Object item) {
-		//
+		for (Node tempNode=head; tempNode!=null;tempNode=tempNode.link) {
+			if(tempNode.data.equals(item))
+				return true;
+		}
 		return false;
 	}
 	
 	public void clear() {
-		//
+		for (Node tempNode=head; tempNode!=null;) {
+			Node nextNode = tempNode.link;
+			tempNode.data = null;
+			tempNode.link = null;
+			tempNode = nextNode;
+		}
+		
 		size = 0;
 		head = null;
 		tail = null;
