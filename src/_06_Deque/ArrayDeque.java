f68c55d17a2b063674283bb7cbb7164f3829515b
LeeHuiJu <leemoon63@gmail.com>
2022-06-12 04:43:34 +0900

    ArrayDeque 메서드 추가, 수정

--- src/_06_Deque/ArrayDeque.java
+++ src/_06_Deque/ArrayDeque.java
@@ -1,5 +1,7 @@
 package _06_Deque;
 
+import java.util.NoSuchElementException;
+
 public class ArrayDeque {
 	private Object[] array; 
 	private int size;
@@ -24,6 +26,8 @@ public class ArrayDeque {
 	}
 	
 	public boolean offerLast(Object item) {
+		if(isFull())
+			throw new IllegalStateException();
 		rear = (rear + 1) % array.length;	
 		array[rear] = item;
 		size++;	
@@ -32,11 +36,51 @@ public class ArrayDeque {
 	}
 	
 	public boolean offerFirst(Object item) {
+		if(isFull())
+			throw new IllegalStateException();
 		array[front] = item;
 		front = (front - 1 + array.length) % array.length;
 		size++;
 		
 		return true;
+	}
+	
+	public Object peek() {
+		return peekFirst();
+	}
+	
+	public Object peekFirst() {
+		if(isEmpty()) {
+			return null;
+		}
+		
+		return array[(front+1)%array.length];
+	}
+	
+	public Object peekLast() {
+		if(isEmpty()) {
+			return null;
+		}
+		
+		return array[(rear)];
+	}
+	
+	public Object element() {
+		return getFirst();
+	}
+	
+	public Object getFirst() {
+		if(isEmpty()) {
+			throw new NoSuchElementException();
+		}
+		return array[(front+1)%array.length];
+	}
+	
+	public Object getLast() {
+		if(isEmpty()) {
+			throw new NoSuchElementException();
+		}
+		return array[(rear)];
 	}
 	
 	public boolean isEmpty() {
